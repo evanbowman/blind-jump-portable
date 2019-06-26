27308d5782386b56f86bc4ac9f6b95f20775055e
evanbowman <ebowman@bu.edu>
2019-06-25 22:20:35 -0400

    bugfix, compilation changes, add Pool

--- source/sprite.hpp
+++ source/sprite.hpp
@@ -5,24 +5,27 @@
 
 class Sprite {
 public:
+    enum class Shape {
+        square,
+        wide,
+        tall
+    };
+
+    enum class Size {
+
+    };
 
     Sprite();
 
-
     Sprite(const Sprite&) = delete;
-
 
     ~Sprite();
 
+    bool initialize();
 
-    bool initialize(int x);
+    void set_position(const Vec2<float>& position);
 
-
-    void set_position(Vec2<float>& position);
-
-
-    const Vec2<float>& get_position() const;
-
+    const Vec2<float> get_position() const;
 
 private:
     volatile void* data_;
