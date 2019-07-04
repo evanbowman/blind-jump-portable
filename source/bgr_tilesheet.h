f599bc24b3f96ac1aae8cdff5ccce6c09154554a
Evan Bowman <evanbowman@Evans-MacBook-Air.local>
2019-07-03 21:27:09 -0400

    commit tilesheet

---
+++ source/bgr_tilesheet.h
@@ -0,0 +1,29 @@
+
+//{{BLOCK(bgr_tilesheet)
+
+//======================================================================
+//
+//	bgr_tilesheet, 96x24@4, 
+//	Transparent color : FF,00,FF
+//	+ palette 256 entries, not compressed
+//	+ 36 tiles Metatiled by 4x3 not compressed
+//	Total size: 512 + 1152 = 1664
+//
+//	Time-stamp: 2019-07-03, 21:23:23
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.15
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_BGR_TILESHEET_H
+#define GRIT_BGR_TILESHEET_H
+
+#define bgr_tilesheetTilesLen 1152
+extern const unsigned int bgr_tilesheetTiles[288];
+
+#define bgr_tilesheetPalLen 512
+extern const unsigned short bgr_tilesheetPal[256];
+
+#endif // GRIT_BGR_TILESHEET_H
+
+//}}BLOCK(bgr_tilesheet)
