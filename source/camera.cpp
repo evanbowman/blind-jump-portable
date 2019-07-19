ac4dacc3e93fe17a3c5ccd1cbc23f60b6b462b86
evanbowman <evan.a.bowman@gmail.com>
2019-07-18 22:01:34 -0400

    update camera weights

--- source/camera.cpp
+++ source/camera.cpp
@@ -10,12 +10,24 @@ void Camera::update(Platform& pfrm,
     auto view = pfrm.screen().get_view();
     const auto& view_center = view.get_center();
 
-    Vec2<Float> target{(seek_pos.x - screen_size.x / 2),
-                       (seek_pos.y - screen_size.y / 2)};
+    Vec2<Float> seek;
+
+    if (ballast_.divisor_) {
+        const auto counter_weight = ballast_.center_ / float(ballast_.divisor_);
+        buffer_ = interpolate(buffer_, counter_weight, 0.000000025f * dt);
+        seek = interpolate(seek_pos, buffer_, 0.78f);
+    } else {
+        seek = seek_pos;
+    }
+
+    Vec2<Float> target{(seek.x - screen_size.x / 2),
+                       (seek.y - screen_size.y / 2)};
 
     view.set_center(interpolate(target, view_center, dt * 0.00000125f));
 
     pfrm.screen().set_view(view);
+
+    ballast_ = {};
 }
 
 
