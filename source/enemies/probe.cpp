57bea404bcc3d5113b26ae57031bace6c658eb7a
evanbowman <ebowman@bu.edu>
2019-08-08 02:51:10 -0400

    migrate project to cmake

---
+++ source/enemies/probe.cpp
@@ -0,0 +1,20 @@
+#include "probe.hpp"
+
+
+Probe::Probe(const Vec2<Float>& position)
+    : hitbox_{&position_, {16, 16}, {8, 8}}
+{
+    position_ = position;
+    sprite_.set_size(Sprite::Size::w16_h32);
+    sprite_.set_origin({8, 8});
+
+    shadow_.set_size(Sprite::Size::w16_h32);
+    shadow_.set_origin({8, 8});
+    shadow_.set_alpha(Sprite::Alpha::translucent);
+}
+
+
+void Probe::update(Platform&, Game&, Microseconds dt)
+{
+
+}
---
+++ source/enemies/probe.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "../entity.hpp"
+#include "../collision.hpp"
+#include "../platform.hpp"
+
+
+class Probe : public Entity<Probe, 20> {
+public:
+    Probe(const Vec2<Float>& position);
+
+    void update(Platform&, Game&, Microseconds);
+
+    const Sprite& get_shadow() const
+    {
+        return shadow_;
+    }
+
+private:
+    Sprite shadow_;
+    HitBox hitbox_;
+};
