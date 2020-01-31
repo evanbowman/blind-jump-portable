bf76e86ae76b7c16826ec2efcd4dba4c433b8656
evanbowman <evan.a.bowman@gmail.com>
2020-01-30 22:37:31 -0500

    superficial changes to misc details

--- source/number/random.hpp
+++ source/number/random.hpp
@@ -19,3 +19,24 @@ inline int random_choice(u32 n)
 {
     return random_value() % n; // * n >> 15;
 }
+
+
+template <u32 offset>
+static Vec2<Float> sample(const Vec2<Float>& position)
+{
+    auto result = position;
+
+    if (random_choice<2>()) {
+        result.x += Float(random_choice<offset>());
+    } else {
+        result.x -= Float(random_choice<offset>());
+    }
+
+    if (random_choice<2>()) {
+        result.y += Float(random_choice<offset>());
+    } else {
+        result.y -= Float(random_choice<offset>());
+    }
+
+    return result;
+}
