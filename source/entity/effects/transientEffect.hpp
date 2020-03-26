88ef4da9145c0ad3ab547de7ed2774c8b17648eb
evanbowman <ebowman@bu.edu>
2020-03-26 00:38:32 -0400

    commit missing transientEffect class to repository

---
+++ source/entity/effects/transientEffect.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+
+#include "graphics/animation.hpp"
+#include "entity/entity.hpp"
+
+
+class Game;
+class Platform;
+
+
+template <TextureIndex InitialTexture, u32 AnimLen, u32 AnimInterval>
+class TransientEffect : public Entity {
+public:
+    TransientEffect() { animation_.bind(sprite_); }
+
+    void update(Platform&, Game&, Microseconds dt)
+    {
+        if (animation_.advance(sprite_, dt) and animation_.done(sprite_)) {
+            this->kill();
+        }
+    }
+
+private:
+    Animation<InitialTexture, AnimLen, AnimInterval> animation_;
+};
