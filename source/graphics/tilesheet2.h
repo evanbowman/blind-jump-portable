9b889e60098dfd574359b199974fab07ad2a1b82
evanbowman <ebowman@bu.edu>
2020-04-18 01:18:26 -0400

    edit tileset

--- source/graphics/tilesheet2.h
+++ source/graphics/tilesheet2.h
@@ -9,7 +9,7 @@
 //	+ 504 tiles Metatiled by 4x3 not compressed
 //	Total size: 512 + 16128 = 16640
 //
-//	Time-stamp: 2020-04-18, 00:00:59
+//	Time-stamp: 2020-04-18, 01:06:27
 //	Exported by Cearn's GBA Image Transmogrifier, v
 //	( http://www.coranac.com/projects/#grit )
 //
