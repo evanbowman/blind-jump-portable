b9e77db10ffdf305182681adcc54831fbffc35f3
evanbowman <ebowman@bu.edu>
2020-05-02 00:42:45 -0400

    change star color back to white

--- source/graphics/overlay_journal.s
+++ source/graphics/overlay_journal.s
@@ -9,7 +9,7 @@
 @	+ 73 tiles not compressed
 @	Total size: 512 + 2336 = 2848
 @
-@	Time-stamp: 2020-05-02, 00:15:05
+@	Time-stamp: 2020-05-02, 00:39:10
 @	Exported by Cearn's GBA Image Transmogrifier, v
 @	( http://www.coranac.com/projects/#grit )
 @
--- source/graphics/tilesheet2.s
+++ source/graphics/tilesheet2.s
@@ -9,7 +9,7 @@
 @	+ 444 tiles Metatiled by 4x3 not compressed
 @	Total size: 512 + 14208 = 14720
 @
-@	Time-stamp: 2020-05-02, 00:15:05
+@	Time-stamp: 2020-05-02, 00:39:10
 @	Exported by Cearn's GBA Image Transmogrifier, v
 @	( http://www.coranac.com/projects/#grit )
 @
