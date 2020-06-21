5d6fe5e0f52344026dfd6ee7bcbb9785408a4ec1
evanbowman <ebowman@bu.edu>
2020-06-21 10:09:48 -0400

    edit scavenger images

--- source/graphics/tilesheet2.h
+++ source/graphics/tilesheet2.h
@@ -9,7 +9,7 @@
 //	+ 444 tiles Metatiled by 4x3 not compressed
 //	Total size: 512 + 14208 = 14720
 //
-//	Time-stamp: 2020-06-20, 22:45:43
+//	Time-stamp: 2020-06-21, 10:04:54
 //	Exported by Cearn's GBA Image Transmogrifier, v
 //	( http://www.coranac.com/projects/#grit )
 //
