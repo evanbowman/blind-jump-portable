0f9055478558541d7f22f5bde092add23f21155d
evanbowman <ebowman@bu.edu>
2020-06-21 14:14:13 -0400

    edit sprites (again)

--- source/graphics/spritesheet.h
+++ source/graphics/spritesheet.h
@@ -9,7 +9,7 @@
 //	+ 1008 tiles Metatiled by 2x4 not compressed
 //	Total size: 512 + 32256 = 32768
 //
-//	Time-stamp: 2020-06-21, 09:32:38
+//	Time-stamp: 2020-06-21, 14:08:26
 //	Exported by Cearn's GBA Image Transmogrifier, v
 //	( http://www.coranac.com/projects/#grit )
 //
