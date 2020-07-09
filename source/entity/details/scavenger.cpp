b11ba0813f6906c6ac7bb20b8bb7da2ace9ed7a8
evanbowman <ebowman@bu.edu>
2020-07-08 20:14:48 -0400

    commit missing files

---
+++ source/entity/details/scavenger.cpp
@@ -0,0 +1,12 @@
+#include "scavenger.hpp"
+
+
+Scavenger::Scavenger(const Vec2<Float>& pos)
+{
+    position_ = pos;
+
+    sprite_.set_size(Sprite::Size::w32_h32);
+    sprite_.set_position(pos);
+    sprite_.set_origin({0, 11});
+    sprite_.set_texture_index(62);
+}
---
+++ source/entity/details/scavenger.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "entity/entity.hpp"
+
+
+class Scavenger : public Entity {
+public:
+    Scavenger(const Vec2<Float>& pos);
+};
