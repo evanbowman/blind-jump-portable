14a747623dee1976677e4239710cff12fb018d3f
evanbowman <ebowman@bu.edu>
2020-08-14 02:51:47 -0400

    commit datetime header

---
+++ source/dateTime.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+
+#include "number/int.h"
+
+
+struct DateTime {
+    s32 year_;
+    s32 month_;
+    s32 day_;
+    s32 hour_;
+    s32 minute_;
+    s32 second_;
+
+    u64 as_seconds() const
+    {
+        auto minute = [](u64 n) { return n * 60; };
+        auto hour = [&minute](u64 n) { return n * minute(60); };
+        auto day = [&hour](u64 n) { return n * hour(24); };
+        auto month = [&day](u64 n) { return n * day(31); }; // FIXME...
+        auto year = [&day](u64 n) { return n * day(365); }; // FIXME...
+
+        return second_ + minute(minute_) + hour(hour_) + day(day_) + month(month_) + year(year_);
+    }
+};
+
+
+
+inline u64 time_diff(const DateTime& before, const DateTime& after)
+{
+    return after.as_seconds() - before.as_seconds();
+}
