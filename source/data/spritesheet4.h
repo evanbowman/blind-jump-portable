ef6dd755ec502f0b85dedc33f5317fcf8dbb5ea7
evanbowman <ebowman@bu.edu>
2020-09-18 22:03:49 -0400

    edit scavenger sprite

--- source/data/spritesheet4.h
+++ source/data/spritesheet4.h
@@ -9,7 +9,7 @@
 //	+ 1008 tiles Metatiled by 2x4 not compressed
 //	Total size: 512 + 32256 = 32768
 //
-//	Time-stamp: 2020-09-18, 18:26:35
+//	Time-stamp: 2020-09-18, 22:01:12
 //	Exported by Cearn's GBA Image Transmogrifier, v0.8.15
 //	( http://www.coranac.com/projects/#grit )
 //
