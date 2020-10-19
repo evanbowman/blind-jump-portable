f7ee1d67ee442a15f7e61cc4b523f2b40c98a28a
evanbowman <ebowman@bu.edu>
2020-10-19 16:17:14 -0400

    fix whitespace bug in lisp interpreter

--- source/script/lisp.cpp
+++ source/script/lisp.cpp
@@ -659,7 +659,8 @@ static u32 eval_expr(const char* expr, u32 len)
     if (i < len) {
         int start = i;
         while (i < len and i < max_fn_name and (not is_whitespace(expr[i])) and
-               expr[i] not_eq ')') {
+               expr[i] not_eq ')' and
+               expr[i] not_eq '(') {
 
             fn_name[i - start] = expr[i];
             ++i;
