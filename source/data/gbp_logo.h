6814be67f42e169682ef793bb003bba3cfe95b72
evanbowman <ebowman@bu.edu>
2020-12-28 10:21:29 -0500

    changes to gbp detection

--- source/data/gbp_logo.h
+++ source/data/gbp_logo.h
@@ -8,7 +8,7 @@
 //	+ bitmap not compressed
 //	Total size: 512 + 38400 = 38912
 //
-//	Time-stamp: 2020-12-27, 17:21:14
+//	Time-stamp: 2020-12-27, 22:06:08
 //	Exported by Cearn's GBA Image Transmogrifier, v0.8.16
 //	( http://www.coranac.com/projects/#grit )
 //
