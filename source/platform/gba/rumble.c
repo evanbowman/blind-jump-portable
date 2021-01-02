a9c4907aff6ebe1660d8815e18914691e919892f
evanbowman <ebowman@bu.edu>
2021-01-02 16:36:03 -0500

    commit missing files

---
+++ source/platform/gba/rumble.c
@@ -0,0 +1,160 @@
+#include "rumble.h"
+
+
+typedef unsigned int u32;
+typedef unsigned short u16;
+
+
+#define GPIO_PORT_DATA        (*(volatile u16 *)0x80000C4)
+#define GPIO_PORT_DIRECTION   (*(volatile u16 *)0x80000C6)
+
+#define REG_BASE 0x04000000
+
+#define REG_RCNT              *(volatile u16*)(REG_BASE + 0x134)
+#define REG_SIOCNT            *(volatile u16*)(REG_BASE + 0x128)
+#define REG_SIODATA32         *(volatile u32*)(REG_BASE + 0x120)
+
+#define SIO_START (1<<7)
+
+
+static char const gbp_comms_stage0_data[] = {"NINTENDO"};
+
+
+static bool gbp_configured;
+static enum RumbleState rumble_state;
+
+
+static struct {
+    u32 serial_in_;
+    u16 stage_;
+    u16 index_;
+    u16 out_0_;
+    u16 out_1_;
+} gbp_comms;
+
+
+static void gbp_serial_isr()
+{
+    u32 out = 0;
+    u16 in_0;
+    u16 in_1;
+
+    gbp_comms.serial_in_ = REG_SIODATA32;
+    switch (gbp_comms.stage_) {
+    case 0:
+        in_0 = gbp_comms.serial_in_ >> 16;
+        in_1 = gbp_comms.serial_in_;
+        if (in_0 == gbp_comms.out_1_) {
+            if (gbp_comms.index_ <= 3) {
+                if (gbp_comms.serial_in_ ==
+                    (u32) ~(gbp_comms.out_1_ | (gbp_comms.out_0_ << 16))) {
+                    gbp_comms.index_++;
+                }
+            } else {
+                if (in_1 == 0x8002) {
+                    out = 0x10000010;
+                    gbp_comms.stage_ = 1;
+                    break;
+                }
+            }
+        } else {
+            gbp_comms.index_ = 0;
+        }
+        if (gbp_comms.index_ <= 3) {
+            gbp_comms.out_0_ =
+                ((const u16*)gbp_comms_stage0_data)[gbp_comms.index_];
+        } else {
+            gbp_comms.out_0_ = 0x8000;
+        }
+        gbp_comms.out_1_ = ~in_1;
+        out = gbp_comms.out_1_ | (gbp_comms.out_0_ << 16);
+        break;
+
+    case 1:
+        if (gbp_comms.serial_in_ == 0x10000010) {
+            out = 0x20000013;
+            gbp_comms.stage_ = 2;
+        } else {
+            gbp_comms.stage_ = 4;
+        }
+        break;
+
+    case 2:
+        if (gbp_comms.serial_in_ == 0x20000013) {
+            out = 0x40000004;
+            gbp_comms.stage_ = 3;
+        } else {
+            gbp_comms.stage_ = 4;
+        }
+        break;
+
+    case 3:
+        if (gbp_comms.serial_in_ == 0x30000003) {
+            out = rumble_state;
+        } else {
+            gbp_comms.stage_ = 4;
+        }
+        break;
+
+    case 4:
+        gbp_comms.serial_in_ = 0;
+        gbp_comms.stage_ = 0;
+        gbp_comms.index_ = 0;
+        gbp_comms.out_0_ = 0;
+        gbp_comms.out_1_ = 0;
+        return;
+    }
+
+    REG_SIODATA32 = out;
+    REG_SIOCNT |= SIO_START;
+}
+
+
+static void gbp_serial_start()
+{
+    if (gbp_comms.serial_in_ != 0x30000003) {
+        REG_RCNT = 0x0;
+        REG_SIOCNT = 0x1008;
+        REG_SIOCNT |= 0x4000;
+        REG_SIOCNT &= ~1;
+        REG_SIOCNT |= 0x0080;
+    }
+}
+
+
+void rumble_init(struct RumbleGBPConfig* config)
+{
+    rumble_state = rumble_stop;
+
+    if (config) {
+        config->serial_irq_setup_(gbp_serial_isr);
+        gbp_configured = true;
+        gbp_comms.serial_in_ = 0;
+        gbp_comms.stage_ = 0;
+        gbp_comms.index_ = 0;
+        gbp_comms.out_0_ = 0;
+        gbp_comms.out_1_ = 0;
+    } else {
+        gbp_configured = false;
+
+    }
+}
+
+
+void rumble_update()
+{
+    if (gbp_configured) {
+        gbp_serial_start();
+    }
+}
+
+
+void rumble_set_state(enum RumbleState state)
+{
+    rumble_state = state;
+
+    if (!gbp_configured) {
+        GPIO_PORT_DIRECTION = 1 << 3;
+        GPIO_PORT_DATA = (rumble_state == rumble_start) << 3;
+    }
+}
---
+++ source/platform/gba/rumble.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <stdbool.h>
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+struct RumbleGBPConfig {
+    void (*serial_irq_setup_)(void (*rumble_isr)(void));
+};
+
+
+enum RumbleState {
+    rumble_start     = 0x40000026,
+    rumble_stop      = 0x40000004,
+    rumble_hard_stop = 0x40000015,
+};
+
+
+void rumble_set_state(enum RumbleState state);
+
+
+void rumble_init(struct RumbleGBPConfig* optional_gbp_config);
+
+
+/* should be called once per frame */
+void rumble_update();
+
+
+#ifdef __cplusplus
+}
+#endif
