b366dfc286c841f9d99ec507dc32270e567fc2ed
evanbowman <ebowman@bu.edu>
2021-01-03 00:36:10 -0500

    cut down size of big sound file

--- source/data/music_chantiers_navals_412.hpp
+++ source/data/music_chantiers_navals_412.hpp
@@ -1,4 +1,3 @@
 #pragma once
-constexpr int music_chantiers_navals_412Len = 3263416;
-extern const unsigned char
-    music_chantiers_navals_412[music_chantiers_navals_412Len];
+constexpr int music_chantiers_navals_412Len = 1154161;
+extern const unsigned char music_chantiers_navals_412[music_chantiers_navals_412Len];
