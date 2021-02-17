caf311020bb0d2bed01b2a162de546fc5ad0141f
evanbowman <ebowman@bu.edu>
2021-02-17 16:55:46 -0500

    store button input stuff

--- source/platform/psp/psp_platform.cpp
+++ source/platform/psp/psp_platform.cpp
@@ -52,10 +52,57 @@ Platform::Platform()
 {
     setup_callbacks();
 
+    sceCtrlSetSamplingCycle(0);
+    sceCtrlSetSamplingMode(PSP_CTRL_MODE_ANALOG);
+
     pspDebugScreenInit();
     pspDebugScreenPrintf("Blind Jump Init");
 
-    while (true) ;
-
     return 0;
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Keyboard
+////////////////////////////////////////////////////////////////////////////////
+
+
+std::optional<Bitvector<int(Key::count)>> missed_keys;
+
+
+void Platform::Keyboard::register_controller(const ControllerInfo& info)
+{
+    // ...
+}
+
+
+void Platform::Keyboard::poll()
+{
+    std::copy(std::begin(states_), std::end(states_), std::begin(prev_));
+
+    SceCtrlData buttonInput;
+
+    sceCtrlPeekBufferPositive(&buttonInput, 1);
+
+    if (buttonInput.Buttons) {
+        states_[int(Key::start)] = buttonInput.Buttons & PSP_CTRL_START;
+        states_[int(Key::select)] = buttonInput.Buttons & PSP_CTRL_SELECT;
+        states_[int(Key::right)] = buttonInput.Buttons & PSP_CTRL_RIGHT;
+        states_[int(Key::left)] = buttonInput.Buttons & PSP_CTRL_LEFT;
+        states_[int(Key::down)] =  buttonInput.Buttons & PSP_CTRL_DOWN;
+        states_[int(Key::up)] = buttonInput.Buttons & PSP_CTRL_UP;
+        states_[int(Key::alt_1)] = buttonInput.Buttons & PSP_CTRL_RTRIGGER;
+        states_[int(Key::alt_2)] = buttonInput.Buttons & PSP_CTRL_LTRIGGER;
+        states_[int(Key::action_1)] = buttonInput.Buttons & PSP_CTRL_CROSS;
+        states_[int(Key::action_2)] = buttonInput.Buttons & PSP_CTRL_CIRCLE;
+    }
+
+    if (UNLIKELY(static_cast<bool>(::missed_keys))) {
+        for (int i = 0; i < (int)Key::count; ++i) {
+            if ((*::missed_keys)[i]) {
+                states_[i] = true;
+            }
+        }
+        ::missed_keys.reset();
+    }
 }
