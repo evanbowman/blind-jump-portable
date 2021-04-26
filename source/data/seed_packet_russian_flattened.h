b454324cb08fb7d6a4d1b0ed308cec5ed9bbffbb
evanbowman <ebowman@bu.edu>
2021-04-26 17:53:43 -0400

    add data file

---
+++ source/data/seed_packet_russian_flattened.h
@@ -0,0 +1,29 @@
+
+//{{BLOCK(seed_packet_russian_flattened)
+
+//======================================================================
+//
+//	seed_packet_russian_flattened, 3816x8@4, 
+//	Transparent color : FF,00,FF
+//	+ palette 256 entries, not compressed
+//	+ 477 tiles not compressed
+//	Total size: 512 + 15264 = 15776
+//
+//	Time-stamp: 2021-04-26, 17:42:32
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.16
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_SEED_PACKET_RUSSIAN_FLATTENED_H
+#define GRIT_SEED_PACKET_RUSSIAN_FLATTENED_H
+
+#define seed_packet_russian_flattenedTilesLen 15264
+extern const unsigned int seed_packet_russian_flattenedTiles[3816];
+
+#define seed_packet_russian_flattenedPalLen 512
+extern const unsigned short seed_packet_russian_flattenedPal[256];
+
+#endif // GRIT_SEED_PACKET_RUSSIAN_FLATTENED_H
+
+//}}BLOCK(seed_packet_russian_flattened)
