0d8a88e0841535f86647822240ad1858a5cc5b89
evanbowman <ebowman@bu.edu>
2021-06-12 09:11:00 -0400

    commit missing files

---
+++ source/data/death_text_russian.h
@@ -0,0 +1,29 @@
+
+//{{BLOCK(death_text_russian)
+
+//======================================================================
+//
+//	death_text_russian, 4032x8@4, 
+//	Transparent color : FF,00,FF
+//	+ palette 256 entries, not compressed
+//	+ 504 tiles not compressed
+//	Total size: 512 + 16128 = 16640
+//
+//	Time-stamp: 2021-05-29, 09:05:29
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.16
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_DEATH_TEXT_RUSSIAN_H
+#define GRIT_DEATH_TEXT_RUSSIAN_H
+
+#define death_text_russianTilesLen 16128
+extern const unsigned int death_text_russianTiles[4032];
+
+#define death_text_russianPalLen 512
+extern const unsigned short death_text_russianPal[256];
+
+#endif // GRIT_DEATH_TEXT_RUSSIAN_H
+
+//}}BLOCK(death_text_russian)
