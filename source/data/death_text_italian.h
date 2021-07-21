20d3123f1bb1bee71c0da42cd4d9f278ea64274c
evanbowman <ebowman@bu.edu>
2021-07-21 19:39:27 -0400

    misc updates to language stuff

---
+++ source/data/death_text_italian.h
@@ -0,0 +1,29 @@
+
+//{{BLOCK(death_text_italian)
+
+//======================================================================
+//
+//	death_text_italian, 4032x8@4, 
+//	Transparent color : FF,00,FF
+//	+ palette 256 entries, not compressed
+//	+ 504 tiles not compressed
+//	Total size: 512 + 16128 = 16640
+//
+//	Time-stamp: 2021-07-21, 18:55:33
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.16
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_DEATH_TEXT_ITALIAN_H
+#define GRIT_DEATH_TEXT_ITALIAN_H
+
+#define death_text_italianTilesLen 16128
+extern const unsigned int death_text_italianTiles[4032];
+
+#define death_text_italianPalLen 512
+extern const unsigned short death_text_italianPal[256];
+
+#endif // GRIT_DEATH_TEXT_ITALIAN_H
+
+//}}BLOCK(death_text_italian)
